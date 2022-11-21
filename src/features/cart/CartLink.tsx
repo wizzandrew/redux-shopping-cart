0a58b2f983495608f1a8f72c376a2df70237ec35
Andrei Varzar <43003757+wizzandrew@users.noreply.github.com>
2022-11-21 23:12:23 +0100

    set up cart to work with redux store

--- src/features/cart/CartLink.tsx
+++ src/features/cart/CartLink.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { getNumItems, getMemoizedNumItems } from "./cartSlice";
+import { useAppSelector } from "../../app/hooks";
 import styles from "./CartLink.module.css";
 
 export function CartLink() {
+  //const numItems = useAppSelector(getNumItems);
+  const numItems = useAppSelector(getMemoizedNumItems);
+  
   return (
     <Link to="/cart" className={styles.link}>
-      <span className={styles.text}>🛒&nbsp;&nbsp;Cart</span>
+      <span className={styles.text}>
+        🛒&nbsp;&nbsp;{numItems ? numItems : "Cart"}</span>
     </Link>
   );
 }
--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -1,4 +1,5 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, createSelector, PayloadAction} from '@reduxjs/toolkit';
+import { RootState } from '../../app/store';
 
 export interface CartState {
     // key of type "string" + value of type "number"
@@ -16,7 +17,39 @@ const initialState: CartState = {
 const cartSlice = createSlice({
     name: "cart",
     initialState,
-    reducers: {}
+    reducers: {
+        addToCart(state, action: PayloadAction<string>) {
+            const id = action.payload;
+            if(state.items[id]) {
+                state.items[id]++;
+            }
+            else {
+                state.items[id] = 1;
+            }
+        }
+    }
 });
 
+export const { addToCart } = cartSlice.actions;
 export default cartSlice.reducer;
+
+export function getNumItems(state: RootState) {
+    console.log("calling getNumItems")
+    let numItems = 0;
+    for(let id in state.cart.items) {
+        numItems += state.cart.items[id];
+    }
+    return numItems;
+}
+
+export const getMemoizedNumItems = createSelector(
+    (state: RootState) => state.cart.items,
+    (items) => {
+        console.log("calling memoized getNumItems")
+        let numItems = 0;
+        for(let id in items) {
+            numItems += items[id];
+        }
+        return numItems;
+    }
+)
